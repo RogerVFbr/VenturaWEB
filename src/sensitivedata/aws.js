11664cb3c1b1604dd3a924ec6e8290a2746bc71c
RogerVFbr <roger.freret@al.infnet.edu.br>
2019-11-11 17:39:52 -0300

    Final API Endpoint

--- src/sensitivedata/aws.js
+++ src/sensitivedata/aws.js
@@ -1,8 +1,17 @@
-export const ADMIN_ENDPOINT_URL = 'https://ducc36yxee.execute-api.us-east-1.amazonaws.com/proto01-01/admin';
-export const REGISTER_ENDPOINT_URL = 'https://ducc36yxee.execute-api.us-east-1.amazonaws.com/proto01-01/register';
-export const DELETE_ENDPOINT_URL = 'https://ducc36yxee.execute-api.us-east-1.amazonaws.com/proto01-01/delete';
-export const BUCKET_URL = 'https://facial-recog-auth-front-images-repo.s3.amazonaws.com/';
-export const API_KEY = 'ihj6N9ICO31VkEW9w1LKra5NltNSujEUa9S57NIz';
+
+// PROTO 01 01
+// export const ADMIN_ENDPOINT_URL = 'https://ducc36yxee.execute-api.us-east-1.amazonaws.com/proto01-01/admin';
+// export const REGISTER_ENDPOINT_URL = 'https://ducc36yxee.execute-api.us-east-1.amazonaws.com/proto01-01/register';
+// export const DELETE_ENDPOINT_URL = 'https://ducc36yxee.execute-api.us-east-1.amazonaws.com/proto01-01/delete';
+// export const BUCKET_URL = 'https://facial-recog-auth-front-images-repo.s3.amazonaws.com/';
+// export const API_KEY = 'ihj6N9ICO31VkEW9w1LKra5NltNSujEUa9S57NIz';
+
+// FELLOW 01 01
+export const ADMIN_ENDPOINT_URL = 'https://unszbb31v7.execute-api.us-east-1.amazonaws.com/fellow-01-01/admin';
+export const REGISTER_ENDPOINT_URL = 'https://unszbb31v7.execute-api.us-east-1.amazonaws.com/fellow-01-01/register';
+export const DELETE_ENDPOINT_URL = 'https://unszbb31v7.execute-api.us-east-1.amazonaws.com/fellow-01-01/delete';
+export const BUCKET_URL = 'https://facial-recog-auth-fellow-01-01-front-images-repo.s3.amazonaws.com/';
+export const API_KEY = 'ijj6N9ICO31VkEW9w1LKra5NltNSujEUa9S57NIz';
 
 
 
